5c17502081ff543a890dc28969e32bcaabc331b1
hittaito-win <310cifra.ggk@gmail.com>
2022-09-26 12:50:45 +0900

    parcel to vite

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 three.js 練習用
 
-| dir  | explain           |
-| :--- | ----------------- |
-| day1 | スクローラー      |
-| day2 | MRT/SDF Texture   |
-| day3 | gaussian blur     |
-| day4 | distort           |
-| day5 | Doom portal [WIP] |
-| day6 | ripple effect     |
-| day7 |
-| day8 | line              |
+| dir  | explain         |
+| :--- | --------------- |
+| day1 | スクローラー    |
+| day2 | MRT/SDF Texture |
+| day3 | gaussian blur   |
+| day4 | line            |
+| day5 | transparent     |
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
+
+let out = {};
+readdirSync(`${__dirname}/src`).forEach((d) => {
+    out = {
+        ...out,
+        [d]: resolve(__dirname, `src/${d}/index.html`),
+    };
+});
+
+export default defineConfig({
+    root: 'src/',
+    plugins: [glsl()],
+    publicDir: '../assets',
+    build: {
+        outDir: '../dist',
+        rollupOptions: {
+            input: out,
+        },
+    },
+});
