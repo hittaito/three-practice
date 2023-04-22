da38fc6f06e5ea468593c9c81184ee382c38522d
hittaito-win <310cifra.ggk@gmail.com>
2023-04-22 13:00:17 +0900

    fix

--- src/day19/Color.ts
+++ src/day19/Color.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export const COLOR = {
     PRIMARY1: new THREE.Color('#2db987'),
-    PRIMARY2: new THREE.Color('#01b777'),
+    PRIMARY2: new THREE.Color('#2CB1BF'),
     PRIMARY3: new THREE.Color('#00935F'),
     PRIMARY4: new THREE.Color('#00935F'),
     PRIMARY5: new THREE.Color('#00935F'),
@@ -10,8 +10,8 @@ export const COLOR = {
     SECONDARY2: new THREE.Color('#FF9701'),
     SECONDARY3: new THREE.Color('#DB8200'),
     SECONDARY4: new THREE.Color('#B26900'),
-    SECONDARY5: new THREE.Color('#875000'),
-    ACCENT1: new THREE.Color('#EC3A66'),
+    SECONDARY5: new THREE.Color('#6F8ABF'),
+    ACCENT1: new THREE.Color('#F2C230'),
     ACCENT2: new THREE.Color('#EC013C'),
     ACCENT3: new THREE.Color('#C80032'),
     ACCENT4: new THREE.Color('#A20029'),
